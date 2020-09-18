7c0b3665c36c57905ae2e49b36e4b1589c9d76bc
Guillaume Perot <guillaume.perot.gerbault@gmail.com>
2020-09-18 15:14:28 +0200

    add navigation and footer

---
+++ components/navigation/Footer.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-footer padless class="footer">
+      <v-card flat class="footer-card">
+        <v-btn icon large class="footer-icon" v-for="icon in icons" :key="icon" :href="icon.url" target="_blank">
+          <v-icon>{{icon.name}}</v-icon>
+        </v-btn>
+        <div class="link-div">
+          <a class="footer-link" v-for="link in links" :key="link" href="https://www.themoviedb.org/documentation/api">{{link}}</a>
+        </div>
+      </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data () {
+    return {
+      icons: [
+         {
+          name: 'mdi-facebook',
+          url: 'https://www.facebook.com/themoviedb/',
+        },
+         {
+          name: 'mdi-twitter',
+          url: 'https://twitter.com/themoviedb?lang=fr',
+        }
+      ],
+      links: [
+        'Application',
+        'Aide',
+        'Mentions Legales'
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.footer {
+  background-color: black;
+  justify-content: center;
+  height: 200px;
+}
+.footer-card {
+  background-color: black !important;
+  width: 100%;
+  text-align: center;
+}
+.footer-icon {
+  color: white !important;
+  margin: 10px 15px;
+}
+.footer-icon:hover {
+  color: #E50914 !important;
+}
+.link-div {
+  margin-top: 40px;
+}
+.footer-link {
+  margin: 10px 15px;
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+  word-break: keep-all;
+}
+
+.footer-link:hover {
+color: #E50914 !important;
+}
+
+.footer-link:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ components/navigation/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app-bar class="app-bar">
+    <v-container fluid>
+      <v-row align="center" class="d-none d-md-flex pl-4" justify="center">
+          <nuxt-link to="/" class="site-name">
+            <div class="logo-frame">
+              <span class="logo-helper"></span><img src="~/assets/logo.svg" width="100" class="logo"><img>
+            </div>
+          </nuxt-link>
+      </v-row>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style>
+</style>
+
+<style scoped>
+.app-bar {
+  background-color: #121212 !important;
+  color: white;
+}
+.site-name {
+  color: #E50914 !important;
+  text-decoration: none !important;
+}
+.logo-frame {
+  height: 33.91px;
+  width: 100px;
+  white-space: nowrap;
+  margin-right: 20px;
+}
+.logo-helper {
+  display: inline-block;
+  height: 100%;
+  vertical-align: middle;
+}
+.logo {
+  vertical-align: middle;
+}
+.menu-button {
+  margin-right: 10px;
+}
+</style>
